e5c59b88a13c31d1c9289a658408ba7ac599d1a4
Andrey Gordienkov <andrei.gordienkov@abax.com>
2023-07-21 19:43:32 +0200

    minimal api

--- DIBasics_Asp/Program.cs
+++ DIBasics_Asp/Program.cs
@@ -1,4 +1,8 @@
+using Lamar.Microsoft.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseLamar();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
